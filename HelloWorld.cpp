4a6c9497fe5e3685eaf9718f79ec002198e2587f
lavanyasle <lavanyasle@gmail.com>
2017-07-05 11:22:53 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+  //The program prints this line onto the console using cout from the standard library.
   cout<<"Hello World! Welcome to Github!\n";
   return 1;
 }
