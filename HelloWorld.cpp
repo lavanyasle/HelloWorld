433880bc5e599fb293c8424244bc77755a56e895
lavanyasle <lavanyasle@gmail.com>
2017-07-05 11:24:07 -0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
   //The program prints this line onto the console using cout from the standard library.
+  //Once the line is printed the cursor moves to a new line because of the new line character (/n).
   cout<<"Hello World! Welcome to Github!\n";
   return 1;
 }
